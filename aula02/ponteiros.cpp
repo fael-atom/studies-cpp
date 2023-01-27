3bc24125a4a3290c3bc8ccf751beae958fece4e5
Rafael Mota <rafaelmota.dev@gmail.com>
2023-01-27 18:32:49 -0300

    Adiciona slides e arquivos aula 2

---
+++ aula02/ponteiros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  /* ALOCAÇÃO ESTÁTICA DE PONTEIRO */
+  int alpha;
+
+  // Variável não inicializada chamada intPointer que aponta para um valor do tipo inteiro e com conteúdo undefined. Ou seja, intPointer é um ponteiro.
+  int* intPointer;
+
+  // O operador & permite obter o endereço de memória de uma variável e inicializar o ponteiro.
+  intPointer = &alpha;
+
+
+  /* ALOCAÇÃO DINÂMICA DE PONTEIRO */
+  
+}
